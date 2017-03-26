9d9434e526fdd3ee7d10d366f713816d5de05bd9
raghavan <oneraghavan@gmail.com>
2017-03-26 09:17:09 +0530

    Finishing the implementation

--- data_structures/TimingWheels/HashedTimingWheels.scala
+++ data_structures/TimingWheels/HashedTimingWheels.scala
@@ -1,33 +1,101 @@
+package firestar.live.metrics
 
-trait AsyncOperation{
+import java.util.Calendar
 
-  val timeToExpire : Int
+import com.sun.javafx.runtime.async.AsyncOperation
+import org.mockito.Mockito.{spy, verify}
+import org.mockito.{InOrder, Mockito}
 
-  def expire : Unit = {}
+import scala.collection.mutable
+
+
+trait AsyncOperation {
+
+  var timeToExpire: Int
+
+  def expire: Unit
 
 }
 
-class DataBaseOperation(expireTime : Int ) extends AsyncOperation{
+class DataBaseOperation(expireTime: Int) extends AsyncOperation {
 
-  timeToExpire = expireTime
+  override var timeToExpire: Int = expireTime
+
+  override def expire: Unit = {}
+}
+
+class TimeOutManager {
+
+  private var currentTick: Int = 1
+
+  private var timeWheel: mutable.Map[Int, List[(Int, AsyncOperation)]] = mutable.Map()
+
+  (0 to 59).foreach((num) => {
+    timeWheel.put(num, List())
+  })
+
+  def addOperation(asyncOperation: AsyncOperation): Unit = {
+    val counter = asyncOperation.timeToExpire / 60
+    val hashKey = asyncOperation.timeToExpire % 60
+
+    timeWheel(hashKey) = timeWheel(hashKey) ++ List((counter, asyncOperation))
+  }
+
+  private def expireOperation: Unit = {
+
+    timeWheel(currentTick) = timeWheel(currentTick).filter(expireTimedOutOperations).map((tuple: ((Int, AsyncOperation))) => {
+      var counter = tuple._1
+      val operation = tuple._2
+
+      return (counter - 1, operation)
+    })
+
+    currentTick = currentTick + 1
+
+  }
+
+  private def expireTimedOutOperations(tuple: ((Int, AsyncOperation))): Boolean = {
+    var counter = tuple._1
+    val operation = tuple._2
+
+    if (counter == 0) {
+      operation.expire
+      return false
+    }
+    true
+
+  }
+
+  def startTimingOut: Unit = {
+    val t = new java.util.Timer()
+    val task = new java.util.TimerTask {
+      def run() = expireOperation
+    }
+    t.scheduleAtFixedRate(task, 0, 1000L)
+  }
 
 }
 
-class TimeOutManager{
+object SomeApp extends App {
 
-  var currentTick : Int = 1
+  private val timeOutManager = new TimeOutManager()
 
-  var timeWheel: Map[Char,AsyncOperation] = Map()
+  val jobExpiringTimes = List(102, 30, 44, 77, 3, 300)
 
-  def addOperation(asyncOperation: AsyncOperation) : Unit = {
+  val job4 = new DataBaseOperation(3)
+  val job1 = new DataBaseOperation(30)
+  val job5 = new DataBaseOperation(35)
+  val job3 = new DataBaseOperation(77)
+  val job2 = new DataBaseOperation(102)
 
-  }
 
-  def expireOperation : Unit = {
+  timeOutManager.addOperation(job1)
+  timeOutManager.addOperation(job2)
+  timeOutManager.addOperation(job3)
+  timeOutManager.addOperation(job4)
+  timeOutManager.addOperation(job5)
 
-    timeWheel
-
-  }
+  timeOutManager.startTimingOut
 
 }
 
